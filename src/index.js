71b919656faf312f25ced18bf0c7cc28f6a38b40
SukiTsu <buibrandon47@gmail.com>
2025-04-30 21:45:46 +0200

    Modification du port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { WebSocketServer } from 'ws';
 
-const server = new WebSocketServer({ port: 3001 });
-console.log('✅ Serveur WebSocket lancé sur ws://localhost:3001');
+const port = process.env.PORT || 3001;
+const server = new WebSocketServer({ port });
+console.log('✅ Serveur WebSocket lancé sur wss://game:3001');
 
 const clients = new Set();
 
